523ac5ddb99ca1c6dd2831a30a64fe456b21152a
H.Sakuma <cao.kuanshen@gmail.com>
2023-11-05 14:11:36 +0900

    tsc

--- docs/src/champ_select.js
+++ docs/src/champ_select.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let selectedChampions = [];
     function fetchData() {
-        return fetch('src/data.json') // JSONファイルのパス
+        return fetch('data.json') // JSONファイルのパス
             .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
